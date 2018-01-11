f73e5685c0caa1bc8d090b70863229f7c278ce9c
jannekai <janne.kaistinen@iki.fi>
2018-01-11 22:39:32 +0200

    Update readme and don't export unset variables

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Or just copy the contents of awse.sh to your .bashrc and/or .zshrc file directly
 
 Suppose you have and ~/.aws/credentials file with the following content:
 
+```
 [dev]
 aws_access_key_id = AKIAI123......
 aws_secret_access_key = abc...........
@@ -17,12 +18,25 @@ aws_secret_access_key = abc...........
 [prod]
 aws_access_key_id = AKIAI999......
 aws_secret_access_key = xyz...........
+```
 
 To switch between dev and prod access keys run the following commands:
 
 ```
 awse dev
+Updated AWS credentials
+AWS_PROFILE: dev
+AWS_ACCESS_KEY_ID: AKIAI123......
+AWS_SECRET_ACCESS_KEY: abc...........
+
 awse prod
+Updated AWS credentials
+AWS_PROFILE: prod
+AWS_ACCESS_KEY_ID: AKIAI999......
+AWS_SECRET_ACCESS_KEY: xyz...........
+
+# To clear env variables run with arguments
+awse
+Clearing AWS env variables AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_ENV
 ```
 
-To clear env variables, simply run _awse_ command without any parameters.
--- awse.sh
+++ awse.sh
@@ -18,7 +18,6 @@ function awse() {
         unset AWS_SECRET_ACCESS_KEY
         unset AWS_PROFILE
         echo "Clearing AWS env variables AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_ENV"
-        export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_ENV
     else
         local KEY_ID=$(read_aws_credential $1 AWS_ACCESS_KEY_ID)
         local ACCESS_KEY=$(read_aws_credential $1 AWS_SECRET_ACCESS_KEY)
@@ -26,14 +25,13 @@ function awse() {
         if [[ $KEY_ID == "" || $ACCESS_KEY == "" ]]; then
             echo "No AWS profile named $1 found or AWS_ACCESS_KEY_ID and/or AWS_SECRET_ACCESS_KEY not set"
         else
-            AWS_ACCESS_KEY_ID=$KEY_ID
-            AWS_SECRET_ACCESS_KEY=$ACCESS_KEY
-            AWS_PROFILE=$1
+            export AWS_ACCESS_KEY_ID=$KEY_ID
+            export AWS_SECRET_ACCESS_KEY=$ACCESS_KEY
+            export AWS_PROFILE=$1
             echo "Updated AWS credentials succesfully:"
             echo "AWS_PROFILE: $AWS_PROFILE"
             echo "AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID"
             echo "AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY[0,5]..."
-            export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_PROFILE
         fi
     fi
 }
